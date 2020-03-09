372bacbd92b6a2bc2ef45d90f32b0ac6e7b570f4
Matuk <mateusmatuk@fai-mg.br>
2020-03-09 19:56:20 -0300

    resposta letra d

--- src/br/fai/projeto/Main.java
+++ src/br/fai/projeto/Main.java
@@ -21,6 +21,28 @@ public class Main {
 		
 		System.out.println("----------");
 		
+		carros[1] = "L200";
+		
+		for (String carro : carros) {
+			System.out.println("Carro: " + carro);
+		}
+		
+		int tamanhoDoNovoArray = carros.length + 1;
+		
+		String[] novoArrayDeCarros = new String[tamanhoDoNovoArray];
+
+		for (int i = 0; i < carros.length; i++) {
+			String carro = carros[i];
+			novoArrayDeCarros[i] = carro;
+		}
+		
+		novoArrayDeCarros[tamanhoDoNovoArray -1] = "Gol";
+		
+		System.out.println("----------");
+		
+		for (String carro : novoArrayDeCarros) {
+			System.out.println("Carro: " + carro);
+		}
 	}
 
 }
